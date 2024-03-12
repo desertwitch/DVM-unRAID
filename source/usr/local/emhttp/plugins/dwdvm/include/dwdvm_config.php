5ba0ac60c7bc23bf3bec7e35e67e3931b47bcdd8
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-03-12 17:33:53 +0100

    changes

--- source/usr/local/emhttp/plugins/dwdvm/include/dwdvm_config.php
+++ source/usr/local/emhttp/plugins/dwdvm/include/dwdvm_config.php
@@ -32,25 +32,25 @@ $dwdvm_primary      = trim(isset($dwdvm_cfg['PRIMARY']) ? htmlspecialchars($dwdv
 $dwdvm_good_notify = isset($dwdvm_cfg['BADNOTIFY'])  ? htmlspecialchars($dwdvm_cfg['BADNOTIFY'])  : 'disable';
 $dwdvm_bad_notify  = isset($dwdvm_cfg['GOODNOTIFY']) ? htmlspecialchars($dwdvm_cfg['GOODNOTIFY']) : 'disable';
 
-$dwdvm_hlimit_rx  = isset($dwdvm_cfg['HLIMITRX'])  ? htmlspecialchars($dwdvm_cfg['HLIMITRX'])  : '-1';
-$dwdvm_hunit_rx   = isset($dwdvm_cfg['HUNITRX'])   ? htmlspecialchars($dwdvm_cfg['HUNITRX'])   : 'GB';
-$dwdvm_dlimit_rx  = isset($dwdvm_cfg['DLIMITRX'])  ? htmlspecialchars($dwdvm_cfg['DLIMITRX'])  : '-1';
-$dwdvm_dunit_rx   = isset($dwdvm_cfg['DUNITRX'])   ? htmlspecialchars($dwdvm_cfg['DUNITRX'])   : 'GB';
-$dwdvm_mlimit_rx  = isset($dwdvm_cfg['MLIMITRX'])  ? htmlspecialchars($dwdvm_cfg['MLIMITRX'])  : '-1';
-$dwdvm_munit_rx  = isset($dwdvm_cfg['MUNITRX'])   ? htmlspecialchars($dwdvm_cfg['MUNITRX'])   : 'GB';
-$dwdvm_ylimit_rx  = isset($dwdvm_cfg['YLIMITRX'])  ? htmlspecialchars($dwdvm_cfg['YLIMITRX'])  : '-1';
-$dwdvm_yunit_rx   = isset($dwdvm_cfg['YUNITRX'])   ? htmlspecialchars($dwdvm_cfg['YUNITRX'])   : 'GB';
+$dwdvm_hlimit_rx  = trim(isset($dwdvm_cfg['HLIMITRX'])  ? htmlspecialchars($dwdvm_cfg['HLIMITRX'])  : '-1');
+$dwdvm_hunit_rx   = trim(isset($dwdvm_cfg['HUNITRX'])   ? htmlspecialchars($dwdvm_cfg['HUNITRX'])   : 'GB');
+$dwdvm_dlimit_rx  = trim(isset($dwdvm_cfg['DLIMITRX'])  ? htmlspecialchars($dwdvm_cfg['DLIMITRX'])  : '-1');
+$dwdvm_dunit_rx   = trim(isset($dwdvm_cfg['DUNITRX'])   ? htmlspecialchars($dwdvm_cfg['DUNITRX'])   : 'GB');
+$dwdvm_mlimit_rx  = trim(isset($dwdvm_cfg['MLIMITRX'])  ? htmlspecialchars($dwdvm_cfg['MLIMITRX'])  : '-1');
+$dwdvm_munit_rx   = trim(isset($dwdvm_cfg['MUNITRX'])   ? htmlspecialchars($dwdvm_cfg['MUNITRX'])   : 'GB');
+$dwdvm_ylimit_rx  = trim(isset($dwdvm_cfg['YLIMITRX'])  ? htmlspecialchars($dwdvm_cfg['YLIMITRX'])  : '-1');
+$dwdvm_yunit_rx   = trim(isset($dwdvm_cfg['YUNITRX'])   ? htmlspecialchars($dwdvm_cfg['YUNITRX'])   : 'GB');
 
-$dwdvm_hlimit_tx  = isset($dwdvm_cfg['HLIMITTX'])  ? htmlspecialchars($dwdvm_cfg['HLIMITTX'])  : '-1';
-$dwdvm_hunit_tx   = isset($dwdvm_cfg['HUNITTX'])   ? htmlspecialchars($dwdvm_cfg['HUNITTX'])   : 'GB';
-$dwdvm_dlimit_tx  = isset($dwdvm_cfg['DLIMITTX'])  ? htmlspecialchars($dwdvm_cfg['DLIMITTX'])  : '-1';
-$dwdvm_dunit_tx   = isset($dwdvm_cfg['DUNITTX'])   ? htmlspecialchars($dwdvm_cfg['DUNITTX'])   : 'GB';
-$dwdvm_mlimit_tx  = isset($dwdvm_cfg['MLIMITTX'])  ? htmlspecialchars($dwdvm_cfg['MLIMITTX'])  : '-1';
-$dwdvm_munit_tx  = isset($dwdvm_cfg['MUNITTX'])   ? htmlspecialchars($dwdvm_cfg['MUNITTX'])   : 'GB';
-$dwdvm_ylimit_tx  = isset($dwdvm_cfg['YLIMITTX'])  ? htmlspecialchars($dwdvm_cfg['YLIMITTX'])  : '-1';
-$dwdvm_yunit_tx   = isset($dwdvm_cfg['YUNITTX'])   ? htmlspecialchars($dwdvm_cfg['YUNITTX'])   : 'GB';
+$dwdvm_hlimit_tx  = trim(isset($dwdvm_cfg['HLIMITTX'])  ? htmlspecialchars($dwdvm_cfg['HLIMITTX'])  : '-1');
+$dwdvm_hunit_tx   = trim(isset($dwdvm_cfg['HUNITTX'])   ? htmlspecialchars($dwdvm_cfg['HUNITTX'])   : 'GB');
+$dwdvm_dlimit_tx  = trim(isset($dwdvm_cfg['DLIMITTX'])  ? htmlspecialchars($dwdvm_cfg['DLIMITTX'])  : '-1');
+$dwdvm_dunit_tx   = trim(isset($dwdvm_cfg['DUNITTX'])   ? htmlspecialchars($dwdvm_cfg['DUNITTX'])   : 'GB');
+$dwdvm_mlimit_tx  = trim(isset($dwdvm_cfg['MLIMITTX'])  ? htmlspecialchars($dwdvm_cfg['MLIMITTX'])  : '-1');
+$dwdvm_munit_tx   = trim(isset($dwdvm_cfg['MUNITTX'])   ? htmlspecialchars($dwdvm_cfg['MUNITTX'])   : 'GB');
+$dwdvm_ylimit_tx  = trim(isset($dwdvm_cfg['YLIMITTX'])  ? htmlspecialchars($dwdvm_cfg['YLIMITTX'])  : '-1');
+$dwdvm_yunit_tx   = trim(isset($dwdvm_cfg['YUNITTX'])   ? htmlspecialchars($dwdvm_cfg['YUNITTX'])   : 'GB');
 
-$dwdvm_running      = (intval(trim(shell_exec( "[ -f /proc/`cat /var/run/vnstat/vnstat.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ))) === 1 );
+$dwdvm_running    = (intval(trim(shell_exec( "[ -f /proc/`cat /var/run/vnstat/vnstat.pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ))) === 1 );
 $dwdvm_installed_backend = trim(shell_exec("find /var/log/packages/ -type f -iname 'vnstat*' -printf '%f\n' 2> /dev/null"));
 
 ?>
