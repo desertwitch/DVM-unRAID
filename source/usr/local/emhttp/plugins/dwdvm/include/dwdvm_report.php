0ce1937ad4e00751c65c25e3c2dbe7e0817d5b90
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-03-13 17:00:27 +0100

    Update dwdvm_report.php

--- source/usr/local/emhttp/plugins/dwdvm/include/dwdvm_report.php
+++ source/usr/local/emhttp/plugins/dwdvm/include/dwdvm_report.php
@@ -20,16 +20,23 @@
  */
 require_once '/usr/local/emhttp/plugins/dwdvm/include/dwdvm_config.php';
 
-function humanFileSize($size,$unit="") {
-    if(intval($size)) {
-        if( (!$unit && $size >= 1<<30) || $unit == "GB")
-            return number_format($size/(1<<30),2)." GB";
-        if( (!$unit && $size >= 1<<20) || $unit == "MB")
-            return number_format($size/(1<<20),2)." MB";
-        if( (!$unit && $size >= 1<<10) || $unit == "KB")
-            return number_format($size/(1<<10),2)." KB";
-        return number_format($size)." B";
-    } else {
+function humanFileSize($sizeObj,$unit="") {
+    try {
+        $size = intval($sizeObj);
+        if($size) {
+            if( (!$unit && $size >= 1<<30) || $unit == "GB")
+                return number_format($size/(1<<30),2)." GB";
+            if( (!$unit && $size >= 1<<20) || $unit == "MB")
+                return number_format($size/(1<<20),2)." MB";
+            if( (!$unit && $size >= 1<<10) || $unit == "KB")
+                return number_format($size/(1<<10),2)." KB";
+            return number_format($size)." B";
+        } else {
+            return "-";
+        }
+    } catch (Throwable $e) { // For PHP 7
+        return "-";
+    } catch (Exception $e) { // For PHP 5
         return "-";
     }
 }
