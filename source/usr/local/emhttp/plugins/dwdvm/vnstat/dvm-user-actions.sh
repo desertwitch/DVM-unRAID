cbd38f1ca1aae25768a72f19a534c38c85ebc07b
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-03-13 10:21:52 +0100

    wordings

--- source/usr/local/emhttp/plugins/dwdvm/vnstat/dvm-user-actions.sh
+++ source/usr/local/emhttp/plugins/dwdvm/vnstat/dvm-user-actions.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
-# NOTE: These actions ONLY EXECUTE when AT LEAST ONE volume-based NOTIFICATION is ENABLED in GUI.
-# NOTE: These actions follow the same behavior as the notifications (execute once when limit changes).
-# NOTE: More advanced scripting can be done in the DVM-CUSTOM-ALARMS.SH which executes once per minute.
+# THIS BASH SCRIPT IS FOR RUNNING BASH COMMANDS TOGETHER WITH VOLUME-BASED NOTIFICATIONS.
+#
+# These actions ONLY EXECUTE when AT LEAST ONE volume-based NOTIFICATION is ENABLED in GUI.
+# These actions follow the same behavior as the notifications - execute once when limit changes.
+# More advanced scripting can be done in the DVM-CUSTOM-ALARMS.SH which executes once per minute.
 #
 
 #
@@ -10,25 +12,25 @@
 #
 
 hour_rx_reached () {
-    echo "user actions for hour_rx_reached executing..." | logger -t "dwm-action"
+    echo "DVM user actions for hour_rx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER HOUR threshold for INBOUND has been EXCEEDED below this line
 
 }
 
 hour_rx_normal_again () {
-    echo "user actions for hour_rx_normal_again executing..." | logger -t "dwm-action"
+    echo "DVM user actions for hour_rx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER HOUR threshold for INBOUND is back to NORMAL below this line
 
 }
 
 hour_tx_reached () {
-    echo "user actions for hour_tx_reached executing..." | logger -t "dwm-action"
+    echo "DVM user actions for hour_tx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER HOUR threshold for OUTBOUND has been EXCEEDED below this line
 
 }
 
 hour_tx_normal_again () {
-    echo "user actions for hour_tx_normal_again executing..." | logger -t "dwm-action"
+    echo "DVM user actions for hour_tx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER HOUR threshold for OUTBOUND is back to NORMAL below this line
 
 }
@@ -38,25 +40,25 @@ hour_tx_normal_again () {
 #
 
 day_rx_reached () {
-    echo "user actions for day_rx_reached executing..." | logger -t "dwm-action"
+    echo "DVM user actions for day_rx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER DAY threshold for INBOUND has been EXCEEDED below this line
 
 }
 
 day_rx_normal_again () {
-    echo "user actions for day_rx_normal_again executing..." | logger -t "dwm-action"
+    echo "DVM user actions for day_rx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER DAY threshold for INBOUND is back to NORMAL below this line
 
 }
 
 day_tx_reached () {
-    echo "user actions for day_tx_reached executing..." | logger -t "dwm-action"
+    echo "DVM user actions for day_tx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER DAY threshold for OUTBOUND has been EXCEEDED below this line
 
 }
 
 day_tx_normal_again () {
-    echo "user actions for day_tx_normal_again executing..." | logger -t "dwm-action"
+    echo "DVM user actions for day_tx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER DAY threshold for OUTBOUND is back to NORMAL below this line
 
 }
@@ -66,25 +68,25 @@ day_tx_normal_again () {
 #
 
 month_rx_reached () {
-    echo "user actions for month_rx_reached executing..." | logger -t "dwm-action"
+    echo "DVM user actions for month_rx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER MONTH threshold for INBOUND has been EXCEEDED below this line
 
 }
 
 month_rx_normal_again () {
-    echo "user actions for month_rx_normal_again executing..." | logger -t "dwm-action"
+    echo "DVM user actions for month_rx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER MONTH threshold for INBOUND is back to NORMAL below this line
 
 }
 
 month_tx_reached () {
-    echo "user actions for month_tx_reached executing..." | logger -t "dwm-action"
+    echo "DVM user actions for month_tx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER MONTH threshold for OUTBOUND has been EXCEEDED below this line
 
 }
 
 month_tx_normal_again () {
-    echo "user actions for month_tx_normal_again executing..." | logger -t "dwm-action"
+    echo "DVM user actions for month_tx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER MONTH threshold for OUTBOUND is back to NORMAL below this line
 
 }
@@ -94,25 +96,25 @@ month_tx_normal_again () {
 #
 
 year_rx_reached () {
-    echo "user actions for year_rx_reached executing..." | logger -t "dwm-action"
+    echo "DVM user actions for year_rx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER YEAR threshold for INBOUND has been EXCEEDED below this line
 
 }
 
 year_rx_normal_again () {
-    echo "user actions for year_rx_normal_again executing..." | logger -t "dwm-action"
+    echo "DVM user actions for year_rx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER YEAR threshold for INBOUND is back to NORMAL below this line
 
 }
 
 year_tx_reached () {
-    echo "user actions for year_tx_reached executing..." | logger -t "dwm-action"
+    echo "DVM user actions for year_tx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER YEAR threshold for OUTBOUND has been EXCEEDED below this line
 
 }
 
 year_tx_normal_again () {
-    echo "user actions for year_tx_normal_again executing..." | logger -t "dwm-action"
+    echo "DVM user actions for year_tx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER YEAR threshold for OUTBOUND is back to NORMAL below this line
 
 }
