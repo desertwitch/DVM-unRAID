3bcf63cc7b138f6af37b1126f2fe2cb5a39f2cdd
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-03-13 13:43:37 +0100

    Revert "var names"
    This reverts commit d60ea660029f752442440c5cc54d1ceff28c05fe.

--- source/usr/local/emhttp/plugins/dwdvm/vnstat/dvm-user-actions.sh
+++ source/usr/local/emhttp/plugins/dwdvm/vnstat/dvm-user-actions.sh
@@ -16,26 +16,26 @@
 # ACTIONS FOR HOURLY VOLUME TRIGGERS
 #
 
-hour_RX_reached () {
-    echo "DVM user actions for hour_RX_reached are now executing..." | logger -t "dvm-action"
+hour_rx_reached () {
+    echo "DVM user actions for hour_rx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER HOUR threshold for INBOUND has been EXCEEDED below this line
 
 }
 
-hour_RX_normal_again () {
-    echo "DVM user actions for hour_RX_normal_again are now executing..." | logger -t "dvm-action"
+hour_rx_normal_again () {
+    echo "DVM user actions for hour_rx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER HOUR threshold for INBOUND is back to NORMAL below this line
 
 }
 
-hour_TX_reached () {
-    echo "DVM user actions for hour_TX_reached are now executing..." | logger -t "dvm-action"
+hour_tx_reached () {
+    echo "DVM user actions for hour_tx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER HOUR threshold for OUTBOUND has been EXCEEDED below this line
 
 }
 
-hour_TX_normal_again () {
-    echo "DVM user actions for hour_TX_normal_again are now executing..." | logger -t "dvm-action"
+hour_tx_normal_again () {
+    echo "DVM user actions for hour_tx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER HOUR threshold for OUTBOUND is back to NORMAL below this line
 
 }
@@ -44,26 +44,26 @@ hour_TX_normal_again () {
 # ACTIONS FOR DAILY VOLUME TRIGGERS
 #
 
-day_RX_reached () {
-    echo "DVM user actions for day_RX_reached are now executing..." | logger -t "dvm-action"
+day_rx_reached () {
+    echo "DVM user actions for day_rx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER DAY threshold for INBOUND has been EXCEEDED below this line
 
 }
 
-day_RX_normal_again () {
-    echo "DVM user actions for day_RX_normal_again are now executing..." | logger -t "dvm-action"
+day_rx_normal_again () {
+    echo "DVM user actions for day_rx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER DAY threshold for INBOUND is back to NORMAL below this line
 
 }
 
-day_TX_reached () {
-    echo "DVM user actions for day_TX_reached are now executing..." | logger -t "dvm-action"
+day_tx_reached () {
+    echo "DVM user actions for day_tx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER DAY threshold for OUTBOUND has been EXCEEDED below this line
 
 }
 
-day_TX_normal_again () {
-    echo "DVM user actions for day_TX_normal_again are now executing..." | logger -t "dvm-action"
+day_tx_normal_again () {
+    echo "DVM user actions for day_tx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER DAY threshold for OUTBOUND is back to NORMAL below this line
 
 }
@@ -72,26 +72,26 @@ day_TX_normal_again () {
 # ACTIONS FOR MONTHLY VOLUME TRIGGERS
 #
 
-month_RX_reached () {
-    echo "DVM user actions for month_RX_reached are now executing..." | logger -t "dvm-action"
+month_rx_reached () {
+    echo "DVM user actions for month_rx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER MONTH threshold for INBOUND has been EXCEEDED below this line
 
 }
 
-month_RX_normal_again () {
-    echo "DVM user actions for month_RX_normal_again are now executing..." | logger -t "dvm-action"
+month_rx_normal_again () {
+    echo "DVM user actions for month_rx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER MONTH threshold for INBOUND is back to NORMAL below this line
 
 }
 
-month_TX_reached () {
-    echo "DVM user actions for month_TX_reached are now executing..." | logger -t "dvm-action"
+month_tx_reached () {
+    echo "DVM user actions for month_tx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER MONTH threshold for OUTBOUND has been EXCEEDED below this line
 
 }
 
-month_TX_normal_again () {
-    echo "DVM user actions for month_TX_normal_again are now executing..." | logger -t "dvm-action"
+month_tx_normal_again () {
+    echo "DVM user actions for month_tx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER MONTH threshold for OUTBOUND is back to NORMAL below this line
 
 }
@@ -100,26 +100,26 @@ month_TX_normal_again () {
 # ACTIONS FOR YEARLY VOLUME TRIGGERS
 #
 
-year_RX_reached () {
-    echo "DVM user actions for year_RX_reached are now executing..." | logger -t "dvm-action"
+year_rx_reached () {
+    echo "DVM user actions for year_rx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER YEAR threshold for INBOUND has been EXCEEDED below this line
 
 }
 
-year_RX_normal_again () {
-    echo "DVM user actions for year_RX_normal_again are now executing..." | logger -t "dvm-action"
+year_rx_normal_again () {
+    echo "DVM user actions for year_rx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER YEAR threshold for INBOUND is back to NORMAL below this line
 
 }
 
-year_TX_reached () {
-    echo "DVM user actions for year_TX_reached are now executing..." | logger -t "dvm-action"
+year_tx_reached () {
+    echo "DVM user actions for year_tx_reached are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER YEAR threshold for OUTBOUND has been EXCEEDED below this line
 
 }
 
-year_TX_normal_again () {
-    echo "DVM user actions for year_TX_normal_again are now executing..." | logger -t "dvm-action"
+year_tx_normal_again () {
+    echo "DVM user actions for year_tx_normal_again are now executing..." | logger -t "dvm-action"
     # Put commands to run when the PER YEAR threshold for OUTBOUND is back to NORMAL below this line
 
 }
